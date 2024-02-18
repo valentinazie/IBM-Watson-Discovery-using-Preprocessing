5ab37392434b0242b2a8991f4a3fe6bcc4359baa
valentinazie <lalala5479@gmail.com>
2024-02-19 02:44:47 +0900

    first commit

--- src/config.py
+++ src/config.py
@@ -4,4 +4,4 @@ RAW_JSON_PATH = './Data/1. WD_output_raw.json'
 FORMATTED_JSON_PATH = './Data/2. WD_output_raw_formatted.json'
 REFORMATTED_JSON_PATH = './Data/3. WD_output_reformatted.json'
 
-project_name = 'SK C&C'
+project_name = ''
